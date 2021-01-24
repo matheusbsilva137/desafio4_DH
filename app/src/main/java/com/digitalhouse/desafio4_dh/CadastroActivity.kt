eae1c625008b34ab7be77d6bf40a0c0f5b64f116
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2021-01-23 23:24:38 -0300

    Adição do fluxo de navegação entre telas com efeitos de transição

--- app/src/main/java/com/digitalhouse/desafio4_dh/CadastroActivity.kt
+++ app/src/main/java/com/digitalhouse/desafio4_dh/CadastroActivity.kt
@@ -1,11 +1,20 @@
 package com.digitalhouse.desafio4_dh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cadastro.*
+import kotlinx.android.synthetic.main.layout_cadastro.view.*
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+
+        inc_cadastro.btn_cadastro.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
     }
 }
--- app/src/main/java/com/digitalhouse/desafio4_dh/GameActivity.kt
+++ app/src/main/java/com/digitalhouse/desafio4_dh/GameActivity.kt
@@ -1,11 +1,20 @@
 package com.digitalhouse.desafio4_dh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        ib_return.setOnClickListener {
+            finish()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+        }
     }
 }
