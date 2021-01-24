c8abf5c55538e985cf15c023d00f3ded424efec8
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2021-01-23 21:23:39 -0300

    Adição do Layout

---
+++ app/src/main/java/com/digitalhouse/desafio4_dh/CadastroGameActivity.kt
@@ -0,0 +1,11 @@
+package com.digitalhouse.desafio4_dh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_game)
+    }
+}
